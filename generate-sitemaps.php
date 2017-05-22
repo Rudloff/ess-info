ffb4e4c6741326ffdf957585ad0d7f8124009eb7
Pierre Rudloff <contact@rudloff.pro>
2017-05-22 22:23:33 +0200

    Detect if generate-sitemaps.php is opened via the browser

--- generate-sitemaps.php
+++ generate-sitemaps.php
@@ -2,30 +2,36 @@
 
 require_once 'vendor/autoload.php';
 
+header('Content-Type: text/plain');
+
 $climate = new League\CLImate\CLImate();
 
-for ($year = 2012; $year <= 2016; $year++) {
-    $climate->flank($year);
-    file_put_contents(
-        __DIR__.'/sitemap-'.$year.'.txt.gz',
-        ''
-    );
-    $json = json_decode(
-        file_get_contents(
-            __DIR__.'/vendor/infogreffe/entreprises-immatriculees-en-'.$year.'/download'
-        )
-    );
-    if ($gzfile = gzopen(__DIR__.'/sitemap-'.$year.'.txt.gz', 'w')) {
-        $progress = $climate->progress()->total(count($json));
-        foreach ($json as $i => $company) {
-            if (isset($company->fields->siren)) {
-                gzwrite(
-                    $gzfile,
-                    'https://ess.netlib.re/company/'.$company->fields->siren.PHP_EOL
-                );
+if (php_sapi_name() == "cli") {
+    for ($year = 2012; $year <= 2016; $year++) {
+        $climate->flank($year);
+        file_put_contents(
+            __DIR__.'/sitemap-'.$year.'.txt.gz',
+            ''
+        );
+        $json = json_decode(
+            file_get_contents(
+                __DIR__.'/vendor/infogreffe/entreprises-immatriculees-en-'.$year.'/download'
+            )
+        );
+        if ($gzfile = gzopen(__DIR__.'/sitemap-'.$year.'.txt.gz', 'w')) {
+            $progress = $climate->progress()->total(count($json));
+            foreach ($json as $i => $company) {
+                if (isset($company->fields->siren)) {
+                    gzwrite(
+                        $gzfile,
+                        'https://ess.netlib.re/company/'.$company->fields->siren.PHP_EOL
+                    );
+                }
+                $progress->current($i);
             }
-            $progress->current($i);
+            gzclose($gzfile);
         }
-        gzclose($gzfile);
     }
+} else {
+    echo 'This script must be used from the CLI.';
 }
